c7e0cc9a6758d492e68d5d918271fbf44d4e74f1
NEWTON MALLICK <NEW99MAL@GMAIL.COM>
2024-11-24 17:35:29 +0000

    Added ConfusionMetrix Ex

---
+++ testing/ConfusionMetrix/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Data.Analysis;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using System.Collections.Generic;
+
+
+var c = new ConfusionMatrix(confusionTableCounts:new double[][],isBinary:false);
+
+var m = new MLContext();
+
+m.MulticlassClassification.Evaluate(new DataFrame());
